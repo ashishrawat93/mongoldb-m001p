0c47278f50cfdce729c86cad01d9e96bc41a2d49
ashishrawat93 <puneashish@yahoo.co.in>
2018-05-30 16:49:31 -0400

    first commit

---
+++ hello_world_bottle.py
@@ -0,0 +1,10 @@
+from bottle import route, run, template
+
+'''Starter code from Bottle, go to localhost:8080 and change the url to "localhost:8080/hello/<NAME>"  '''
+
+@route('/hello/<name>')
+def index(name):
+    return template('<b>Hello {{name}}</b>!', name=name)
+
+
+run(host='localhost', port=8080)
