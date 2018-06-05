6fddf85a100c0a1ae7d89a51b192285cac9ddf67
ashishrawat93 <puneashish@yahoo.co.in>
2018-06-05 18:14:48 -0400

    getting data from reddit

---
+++ read_reddit.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import json
+import urllib2
+import pymongo
+import certifi
+
+# connect to mongo
+connection = pymongo.MongoClient("mongodb://localhost")
+
+# get a handle to the reddit database
+db = connection.reddit
+stories = db.stories
+
+# drop existing collection
+stories.drop()
+
+# get the reddit home page
+reddit_page = urllib2.urlopen("http://www.reddit.com/r/technology/.json", cafile=certifi.where())
+
+# parse the json into python objects
+parsed = json.loads(reddit_page.read())
+
+# iterate through every news item on the page
+for item in parsed['data']['children']:
+    # put it in mongo
+    stories.insert_one(item['data'])
