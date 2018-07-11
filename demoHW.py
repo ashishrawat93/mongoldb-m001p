74ec0c2b3bf8339466e06bab574f0c0328f5cedc
ashishrawat93 <puneashish@yahoo.co.in>
2018-07-11 11:59:31 +0530

    final_exam_template

--- demoHW.py
+++ demoHW.py
@@ -5,8 +5,8 @@ import pymongo
 connection = pymongo.MongoClient("mongodb://localhost")
 
 # get a handle to the school database
-db = connection.school
-students = db.students
+db = connection.enron
+messages = db.messages
 
 
 def find():
@@ -14,20 +14,14 @@ def find():
 
 
     try:
-        cursor = students.find({})
+        cursor = messages.find({"headers.From":"andrew.fastow@enron.com", "headers.To":"jeff.skilling@enron.com"})
+
     except Exception as e:
         print "Unexpected error:", type(e), e
     all_ids = []
     for doc in cursor:
-        min = float('inf')
-        idx = None
-        hw = doc['scores']
-        for jdx, each_hw in enumerate(hw):
-            if each_hw['type'] == 'homework' and each_hw['score']<=min:
-                min = each_hw['score']
-                idx = jdx
-        doc['scores'].pop(idx)
-        students.replace_one({'_id':doc['_id']}, doc)
+        print(doc)
+    print((cursor.count(True)))
 
 
 
